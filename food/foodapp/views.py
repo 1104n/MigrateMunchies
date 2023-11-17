ff2045cd004f9590c07326906914b2c8f8b75e88
Naveen <naveen@vr1tel.com>
2023-11-17 11:39:23 +0530

    login page sample

--- food/foodapp/views.py
+++ food/foodapp/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def store(request) :
+def store(request):
     context = {}
     return render(request, 'store/store.html', context)
 
 
-def cart(request) :
+def cart(request):
     context = {}
     return render(request, 'store/cart.html', context)
 
 
-def checkout(request) :
+def checkout(request):
     context = {}
     return render(request, 'store/checkout.html', context)
+
+
+def login(request):
+    context = {}
+    return render(request, 'registration/login.html', context)
