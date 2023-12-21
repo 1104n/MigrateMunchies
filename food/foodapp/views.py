f730bb45a152bc6105bb5fcb7ca7b01f0fd73545
Naveen <naveen@vr1tel.com>
2023-12-21 10:15:07 +0530

    updation in login and register

--- food/foodapp/views.py
+++ food/foodapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
-from django.urls import reverse
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.contrib.auth import login, authenticate, logout
